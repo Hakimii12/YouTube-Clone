f07dab2107eb5fc09d28eac3cdb216e8bd5a16ff
Hakimii12 <117950633+Hakimii12@users.noreply.github.com>
2024-09-18 07:29:02 +0300

    styling

--- YouTube-Clone/src/Component/navbar.jsx
+++ YouTube-Clone/src/Component/navbar.jsx
@@ -5,7 +5,7 @@ import search_icon from '../assets/search.png'
 import upload_icon from '../assets/upload.png'
 import more_icon from '../assets/more.png'
 import notification_icon from '../assets/notification.png'
-import profile_icon from '../assets/user_profile.jpg'
+import profile_icon from '../assets/jack.png'
 import '../Component/navbar.css'
 function Navbar() {
   return (
@@ -17,10 +17,12 @@ function Navbar() {
         <div className="flexmiddle flex-div">
         <div className='searchBox flex-div'>
             <input type="text" placeholder='search' className='searchEngne'/>
-             <img src={search_icon} className='searchEngneIcon' />
+            <div className="searchIcon-bar">
+            <img src={search_icon} className='searchEngneIcon' />
+            </div>
         </div>
         </div>
-        <div className="flexright">
+        <div className="flexright flex-div">
             <img src={upload_icon} alt="" />
             <img src={more_icon} alt="" />
             <img src={notification_icon} alt="" />
