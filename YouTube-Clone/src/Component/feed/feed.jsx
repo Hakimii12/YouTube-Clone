516ae2583df6a7d59f4aa71fa52dbdb635ffba8f
Hakimii12 <117950633+Hakimii12@users.noreply.github.com>
2024-10-04 06:47:01 +0300

    feeds/posted video page

---
+++ YouTube-Clone/src/Component/feed/feed.jsx
@@ -0,0 +1,144 @@
+import React from 'react'
+import '../feed/feed.css'
+import thumbnail1 from "../../assets/thumbnail1.png"
+import thumbnail2 from '../../assets/thumbnail2.png'
+import thumbnail3 from '../../assets/thumbnail3.png'
+import thumbnail4 from '../../assets/thumbnail4.png'
+import thumbnail5 from '../../assets/thumbnail5.png'
+import thumbnail6 from '../../assets/thumbnail6.png'
+import thumbnail7 from '../../assets/thumbnail7.png'
+import thumbnail8 from '../../assets/thumbnail8.png'
+function feed() {
+  return (
+    <div className='feed'>
+      <div className="cart">
+        <img src={thumbnail1} alt="" />
+        <h2>Best cchannel to learn coding that help you to a web developer</h2>
+        <h3>Greatstack</h3>
+        <p>15k view &bull; 2 days ago</p>
+      </div>
+      <div className="cart">
+        <img src={thumbnail2} alt="" />
+        <h2>Best cchannel to learn coding that help you to a web developer</h2>
+        <h3>Greatstack</h3>
+        <p>15k view &bull; 2 days ago</p>
+      </div>
+      <div className="cart">
+        <img src={thumbnail3} alt="" />
+        <h2>Best cchannel to learn coding that help you to a web developer</h2>
+        <h3>Greatstack</h3>
+        <p>15k view &bull; 2 days ago</p>
+      </div>
+      <div className="cart">
+        <img src={thumbnail4} alt="" />
+        <h2>Best cchannel to learn coding that help you to a web developer</h2>
+        <h3>Greatstack</h3>
+        <p>15k view &bull; 2 days ago</p>
+      </div>
+      <div className="cart">
+        <img src={thumbnail5} alt="" />
+        <h2>Best cchannel to learn coding that help you to a web developer</h2>
+        <h3>Greatstack</h3>
+        <p>15k view &bull; 2 days ago</p>
+      </div>
+      <div className="cart">
+        <img src={thumbnail6} alt="" />
+        <h2>Best cchannel to learn coding that help you to a web developer</h2>
+        <h3>Greatstack</h3>
+        <p>15k view &bull; 2 days ago</p>
+      </div>
+      <div className="cart">
+        <img src={thumbnail7} alt="" />
+        <h2>Best cchannel to learn coding that help you to a web developer</h2>
+        <h3>Greatstack</h3>
+        <p>15k view &bull; 2 days ago</p>
+      </div>
+      <div className="cart">
+        <img src={thumbnail1} alt="" />
+        <h2>Best cchannel to learn coding that help you to a web developer</h2>
+        <h3>Greatstack</h3>
+        <p>15k view &bull; 2 days ago</p>
+      </div>
+      <div className="cart">
+        <img src={thumbnail2} alt="" />
+        <h2>Best cchannel to learn coding that help you to a web developer</h2>
+        <h3>Greatstack</h3>
+        <p>15k view &bull; 2 days ago</p>
+      </div>
+      <div className="cart">
+        <img src={thumbnail3} alt="" />
+        <h2>Best cchannel to learn coding that help you to a web developer</h2>
+        <h3>Greatstack</h3>
+        <p>15k view &bull; 2 days ago</p>
+      </div>
+      <div className="cart">
+        <img src={thumbnail4} alt="" />
+        <h2>Best cchannel to learn coding that help you to a web developer</h2>
+        <h3>Greatstack</h3>
+        <p>15k view &bull; 2 days ago</p>
+      </div>
+      <div className="cart">
+        <img src={thumbnail5} alt="" />
+        <h2>Best cchannel to learn coding that help you to a web developer</h2>
+        <h3>Greatstack</h3>
+        <p>15k view &bull; 2 days ago</p>
+      </div>
+      <div className="cart">
+        <img src={thumbnail6} alt="" />
+        <h2>Best cchannel to learn coding that help you to a web developer</h2>
+        <h3>Greatstack</h3>
+        <p>15k view &bull; 2 days ago</p>
+      </div>
+      <div className="cart">
+        <img src={thumbnail7} alt="" />
+        <h2>Best cchannel to learn coding that help you to a web developer</h2>
+        <h3>Greatstack</h3>
+        <p>15k view &bull; 2 days ago</p>
+      </div>
+      <div className="cart">
+        <img src={thumbnail1} alt="" />
+        <h2>Best cchannel to learn coding that help you to a web developer</h2>
+        <h3>Greatstack</h3>
+        <p>15k view &bull; 2 days ago</p>
+      </div>
+      <div className="cart">
+        <img src={thumbnail2} alt="" />
+        <h2>Best cchannel to learn coding that help you to a web developer</h2>
+        <h3>Greatstack</h3>
+        <p>15k view &bull; 2 days ago</p>
+      </div>
+      <div className="cart">
+        <img src={thumbnail3} alt="" />
+        <h2>Best cchannel to learn coding that help you to a web developer</h2>
+        <h3>Greatstack</h3>
+        <p>15k view &bull; 2 days ago</p>
+      </div>
+      <div className="cart">
+        <img src={thumbnail4} alt="" />
+        <h2>Best cchannel to learn coding that help you to a web developer</h2>
+        <h3>Greatstack</h3>
+        <p>15k view &bull; 2 days ago</p>
+      </div>
+      <div className="cart">
+        <img src={thumbnail5} alt="" />
+        <h2>Best cchannel to learn coding that help you to a web developer</h2>
+        <h3>Greatstack</h3>
+        <p>15k view &bull; 2 days ago</p>
+      </div>
+      <div className="cart">
+        <img src={thumbnail6} alt="" />
+        <h2>Best cchannel to learn coding that help you to a web developer</h2>
+        <h3>Greatstack</h3>
+        <p>15k view &bull; 2 days ago</p>
+      </div>
+      <div className="cart">
+        <img src={thumbnail7} alt="" />
+        <h2>Best cchannel to learn coding that help you to a web developer</h2>
+        <h3>Greatstack</h3>
+        <p>15k view &bull; 2 days ago</p>
+      </div>
+    </div>
+  )
+}
+
+export default feed
--- YouTube-Clone/src/pages/Home/home.jsx
+++ YouTube-Clone/src/pages/Home/home.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import Sidebar from '../../Sidebar/sidebar'
+import Feed from '../../Component/feed/feed'
+import '../Home/home.css'
 function Home({sidebar}) {
   return (
     <div>
       <Sidebar sidebar={sidebar}/>
+      <div className={`container${sidebar ?"":"large-container"}`}>
+        <Feed/>
+      </div>
     </div>
   )
 }
