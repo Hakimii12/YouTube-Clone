5e160787b00ee8f76e4e2f2a5a7d9a446f445de1
Hakimii12 <117950633+Hakimii12@users.noreply.github.com>
2024-10-09 06:39:34 +0300

    video-play integration

--- YouTube-Clone/src/Component/video-Playe/video-playe.jsx
+++ YouTube-Clone/src/Component/video-Playe/video-playe.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../video-Playe/videoPlaye.css'
 import video1 from '../../assets/video.mp4'
 import like from '../../assets/like.png'
@@ -7,17 +7,27 @@ import share from '../../assets/share.png'
 import jack from '../../assets/jack.png'
 import save from '../../assets/save.png'
 import user_profile from '../../assets/user_profile.jpg'
+import API_KEY from '../../data'
+import moment from 'moment'
+import Converter from '../../Converter'
 function PlayeVideo({id}) {
+   const [videoinfo,setVideoinfo]=useState(null)
+   const fetchdata=async()=>{
+     const vidInfo_url=`https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=Ks-_Mh1QhMc&key=${API_KEY}`
+     await fetch(vidInfo_url).then(res=>res.json()).then(Data=>setVideoinfo(Data.items[0]))
+   }
+   useEffect(()=>{
+     fetchdata()
+   },[id])
   return (
     <div className='play-videos'>
-      {/* <video src={video1} controls autoPlay ></video> */}
-      <iframe src={`https://www.youtube.com/embed/${id}`} frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
-      <h3>Best YouTube Channel To Learn Web Development</h3>
+      <iframe src={`https://www.youtube.com/embed/${id}?autoplay`} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      <h3>{videoinfo?videoinfo.snippet.title:'Video Title Not Available'}</h3>
       <div className="video-info">
-      <p>15k view &bull; 2 days ago</p>
+      <p>{Converter(videoinfo.statistics.viewCount)} &bull; {moment(videoinfo.snippet.publishedAt).fromNow()}</p>
       <div className="video-info-icon">
-        <img src={like} alt="" /><span> 348</span>
-        <img src={dislike} alt="" /><span>3</span>
+        <img src={like} alt="" /><span> {Converter(videoinfo.statistics.likeCount)}</span>
+        <img src={dislike} alt="" /><span></span>
         <img src={share} alt="" /><span>share</span>
         <img src={save} alt="" /><span>save</span>
       </div>
@@ -25,7 +35,7 @@ function PlayeVideo({id}) {
       <hr />
       <div className="video-profileinfo">
         <div className="about-author">
-        <img src={jack} alt="" />
+        <img src={videoinfo.snippet.thumbnails.medium.url} alt="" />
         <div className="about-profile">
         <h3>GreatStack</h3>
         <p>1M Subscribers</p>
@@ -34,10 +44,9 @@ function PlayeVideo({id}) {
         <button>Subscribe</button>
       </div>
       <div className="video-discription">
-        <p>channel that makes learning Easy</p>
-        <p>Subscribe Greatstack to watch more tutorials on web Development</p>
+        <p>{videoinfo?videoinfo.snippet.description:'Video Descritption Not Available'}</p>
         <hr />
-        <p>130 Comments</p>
+        <p>{Converter(videoinfo.statistics.commentCount)} Comments</p>
 
         <div className="comment-section">
         <img src={user_profile} alt="" />
