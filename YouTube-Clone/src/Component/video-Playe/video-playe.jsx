b7ad2c41ddb58022c2ccd5d09afabe84771dc662
Hakimii12 <117950633+Hakimii12@users.noreply.github.com>
2024-10-10 04:07:56 +0300

    indegrating Api

--- YouTube-Clone/src/Component/video-Playe/video-playe.jsx
+++ YouTube-Clone/src/Component/video-Playe/video-playe.jsx
@@ -12,21 +12,39 @@ import moment from 'moment'
 import Converter from '../../Converter'
 function PlayeVideo({id}) {
    const [videoinfo,setVideoinfo]=useState(null)
-   const fetchdata=async()=>{
-     const vidInfo_url=`https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=Ks-_Mh1QhMc&key=${API_KEY}`
-     await fetch(vidInfo_url).then(res=>res.json()).then(Data=>setVideoinfo(Data.items[0]))
+   const [channelinfo,setChannelinfo]=useState(null)
+   const [commentinfo,setCommentinfo]=useState(null)
+   const fetchVideodata=async()=>{
+     const vidInfo_url=`https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${id}&key=${API_KEY}`
+     await fetch(vidInfo_url).then(res=>res.json()).then(data=>setVideoinfo(data.items[0]))
+   
+   }
+   const fetchChannelinfo=async()=>{
+      const channelInfo_url=`https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=${videoinfo.snippet.channelId}&key=${API_KEY}`
+      await fetch(channelInfo_url).then(res=>res.json()).then(Data=>setChannelinfo(Data.items[0]))
+   }
+   const fetchCommentinfo=async()=>{
+      const commentInfo_url=`https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet%2Creplies&maxResults=50&videoId=${Id}&key=${API_KEY}`
+      await fetch(commentInfo_url).then(res=>res.json).then(data=>setCommentinfo(data.items[0]))
    }
    useEffect(()=>{
-     fetchdata()
-   },[id])
+     fetchVideodata()
+     console.log(videoinfo)
+   },[])
+   useEffect(()=>{
+      fetchChannelinfo()
+   },[videoinfo])
+   useEffect(()=>{
+      fetchCommentinfo()
+   },[])
   return (
     <div className='play-videos'>
-      <iframe src={`https://www.youtube.com/embed/${id}?autoplay`} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      <iframe src={`https://www.youtube.com/embed/${id}`} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
       <h3>{videoinfo?videoinfo.snippet.title:'Video Title Not Available'}</h3>
       <div className="video-info">
-      <p>{Converter(videoinfo.statistics.viewCount)} &bull; {moment(videoinfo.snippet.publishedAt).fromNow()}</p>
+      <p>{videoinfo?Converter(videoinfo.statistics.viewCount):"information is not provided"} &bull; {videoinfo?moment(videoinfo.snippet.publishedAt).fromNow():"information is not provided"}</p>
       <div className="video-info-icon">
-        <img src={like} alt="" /><span> {Converter(videoinfo.statistics.likeCount)}</span>
+        <img src={like} alt="" /><span> {videoinfo?Converter(videoinfo.statistics.likeCount):"information is not provided"}</span>
         <img src={dislike} alt="" /><span></span>
         <img src={share} alt="" /><span>share</span>
         <img src={save} alt="" /><span>save</span>
@@ -35,10 +53,10 @@ function PlayeVideo({id}) {
       <hr />
       <div className="video-profileinfo">
         <div className="about-author">
-        <img src={videoinfo.snippet.thumbnails.medium.url} alt="" />
+        <img src={videoinfo?videoinfo.snippet.thumbnails.medium.url:"information is not provided"} alt="" />
         <div className="about-profile">
-        <h3>GreatStack</h3>
-        <p>1M Subscribers</p>
+        <h3>{videoinfo?videoinfo.snippet.channelTitle:"information is not provided"}</h3>
+        <p>{channelinfo?Converter(channelinfo.statistics.subscriberCount):"information is not provided"}</p>
         </div>
         </div>
         <button>Subscribe</button>
@@ -46,119 +64,25 @@ function PlayeVideo({id}) {
       <div className="video-discription">
         <p>{videoinfo?videoinfo.snippet.description:'Video Descritption Not Available'}</p>
         <hr />
-        <p>{Converter(videoinfo.statistics.commentCount)} Comments</p>
-
-        <div className="comment-section">
-        <img src={user_profile} alt="" />
-        <div className="comment-discription">
-        <h4>jack Nicholas <span>1 day ago</span></h4>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-           Nesciunt, eveniet ea quidem omnis cum adipisci amet 
-           </p>
-        </div>
-        </div>
-        <div className="video-info-icon">
-              <img src={like} alt="" /><span> 348</span>
-              <img src={dislike} alt="" />
-         </div>
- 
-         <div className="comment-section">
-        <img src={user_profile} alt="" />
-        <div className="comment-discription">
-        <h4>jack Nicholas <span>1 day ago</span></h4>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-           Nesciunt, eveniet ea quidem omnis cum adipisci amet 
-           </p>
-        </div>
-        </div>
-        <div className="video-info-icon">
-              <img src={like} alt="" /><span> 348</span>
-              <img src={dislike} alt="" />
-         </div> 
-
-         <div className="comment-section">
-        <img src={user_profile} alt="" />
-        <div className="comment-discription">
-        <h4>jack Nicholas <span>1 day ago</span></h4>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-           Nesciunt, eveniet ea quidem omnis cum adipisci amet 
-           </p>
-        </div>
-        </div>
-        <div className="video-info-icon">
-              <img src={like} alt="" /><span> 348</span>
-              <img src={dislike} alt="" />
-         </div>
-
-         <div className="comment-section">
-        <img src={user_profile} alt="" />
-        <div className="comment-discription">
-        <h4>jack Nicholas <span>1 day ago</span></h4>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-           Nesciunt, eveniet ea quidem omnis cum adipisci amet 
-           </p>
-        </div>
-        </div>
-        <div className="video-info-icon">
-              <img src={like} alt="" /><span> 348</span>
-              <img src={dislike} alt="" />
-         </div>
-
-         <div className="comment-section">
-        <img src={user_profile} alt="" />
-        <div className="comment-discription">
-        <h4>jack Nicholas <span>1 day ago</span></h4>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-           Nesciunt, eveniet ea quidem omnis cum adipisci amet 
-           </p>
-        </div>
-        </div>
-        <div className="video-info-icon">
-              <img src={like} alt="" /><span> 348</span>
-              <img src={dislike} alt="" />
-         </div>
-
-         <div className="comment-section">
-        <img src={user_profile} alt="" />
-        <div className="comment-discription">
-        <h4>jack Nicholas <span>1 day ago</span></h4>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-           Nesciunt, eveniet ea quidem omnis cum adipisci amet 
-           </p>
-        </div>
-        </div>
-        <div className="video-info-icon">
-              <img src={like} alt="" /><span> 348</span>
-              <img src={dislike} alt="" />
-         </div>
-
-         <div className="comment-section">
-        <img src={user_profile} alt="" />
-        <div className="comment-discription">
-        <h4>jack Nicholas <span>1 day ago</span></h4>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-           Nesciunt, eveniet ea quidem omnis cum adipisci amet 
-           </p>
-        </div>
-        </div>
-        <div className="video-info-icon">
-              <img src={like} alt="" /><span> 348</span>
-              <img src={dislike} alt="" />
-         </div>
-
-         <div className="comment-section">
-        <img src={user_profile} alt="" />
-        <div className="comment-discription">
-        <h4>jack Nicholas <span>1 day ago</span></h4>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-           Nesciunt, eveniet ea quidem omnis cum adipisci amet 
-           </p>
-        </div>
-        </div>
-        <div className="video-info-icon">
-              <img src={like} alt="" /><span> 348</span>
-              <img src={dislike} alt="" />
-         </div>
+        <p>{videoinfo?Converter(videoinfo.statistics.commentCount):"information is not provided"} Comments</p>
+        {commentinfo.map((data,index)=>{
+         return(
+         <><div className="comment-section">
+            <img src={user_profile} alt="" />
+            <div className="comment-discription">
+            <h4>jack Nicholas <span>1 day ago</span></h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+               Nesciunt, eveniet ea quidem omnis cum adipisci amet 
+               </p>
+            </div>
+            </div>
+            <div className="video-info-icon">
+                  <img src={like} alt="" /><span> 348</span>
+                  <img src={dislike} alt="" />
+             </div>
+         </>)
+        })}
+        
       </div>
     </div>
   )
